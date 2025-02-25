1eb8fabb98e1595cb7c09386573c04e0ca75bba6
shiroxxsora <gagarin.74.d@gmail.com>
2025-02-25 18:38:12 +0800

    and one more

--- render.com.start.sh
+++ render.com.start.sh
@@ -3,5 +3,16 @@
 echo "Running migrations..."
 python stripe_demo/manage.py migrate --noinput
 
+echo "Creating superuser..."
+python stripe_demo/manage.py createsuperuser --noinput --username root --email admin@example.com
+
+python stripe_demo/manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+user = User.objects.get(username='root')
+user.set_password('password123')
+user.save()
+EOF
+
 echo "Starting server..."
 python stripe_demo/manage.py runserver 0.0.0.0:$PORT
