e4750563acc326ba3e9fec0d544c0aa3a21051a0
shiroxxsora <gagarin.74.d@gmail.com>
2025-02-25 18:21:11 +0800

    edit render.com startup file

--- render.com.start.sh
+++ render.com.start.sh
@@ -3,8 +3,5 @@
 echo "Running migrations..."
 python stripe_demo/manage.py migrate --noinput
 
-echo "Collecting static files..."
-python stripe_demo/manage.py collectstatic --noinput
-
 echo "Starting Gunicorn server..."
-gunicorn stripe_demo.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn stripe_demo/manage.py runserver 0.0.0.0:$PORT
